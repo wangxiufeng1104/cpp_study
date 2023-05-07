306c99809cf53c11351b2d188539fafd31ddc8a8
arno <1017845778@qq.com>
2023-05-07 21:38:07 +0800

    【containers】增加笔记

--- cpp_stl/containers/containers.md
+++ cpp_stl/containers/containers.md
@@ -2,6 +2,9 @@
 ## 结构分类
 - 顺序容器
 - 关联容器（associative - container）
+- 无序容器
+
+![image-20230507164231579](containers.assets/image-20230507164231579.png)
 
 ### 顺序容器
 
@@ -25,3 +28,43 @@ C++中有三种关联容器：`std::map`、`std::set` 和 `std::multimap`/`std::
 3. `std::multimap`和`std::multiset`也是关联容器，它们与`std::map`和`std::set`的区别在于，它们允许存储重复的元素。`std::multimap`和`std::multiset`内部实现也是使用红黑树，但它们可以存储相同的键或值。如果插入重复的键或值，`std::multimap`和`std::multiset`会将它们插入到容器中。
 
 总的来说，关联容器适用于需要快速查找元素的场景。`std::map`和`std::multimap`适用于需要根据关键字查找元素的场景，而`std::set`和`std::multiset`适用于需要快速查找元素并且不需要重复元素的场景。
+
+### 无序容器
+
+C++中有四种无序容器：`std::unordered_set`、`std::unordered_map`、`std::unordered_multiset`和`std::unordered_multimap`。
+
+这些容器在内部使用<font color='red'>哈希表（hash table）</font>来存储元素，因此支持常数时间（平均情况下）的查找、插入和删除操作。
+
+1. `std::unordered_set`是一个集合容器，存储一组不重复的元素。`std::unordered_set`中的元素不按照任何顺序存储，并且不能通过下标访问元素。
+2. `std::unordered_map`是一个关联容器，存储一组键值对。`std::unordered_map`中的元素不按照任何顺序存储，并且可以通过键来访问元素。对于每个键，`std::unordered_map`中最多只能有一个对应的值。
+3. `std::unordered_multiset`是一个集合容器，存储一组元素，可以有重复元素。`std::unordered_multiset`中的元素不按照任何顺序存储，并且不能通过下标访问元素。
+4. `std::unordered_multimap`是一个关联容器，存储一组键值对，可以有重复键和值。`std::unordered_multimap`中的元素不按照任何顺序存储，并且可以通过键来访问元素。对于每个键，`std::unordered_multimap`中可以有多个对应的值。
+
+<u>需要注意的是，由于哈希表是基于哈希函数来实现的，因此无序容器中的元素并不保证按照任何特定的顺序存储。此外，哈希表的实现可能会导致性能下降，例如在哈希冲突（hash collision）较多的情况下，哈希表的性能会下降。</u>
+
+## 容器遍历
+
+C++11提供了两种遍历容器的方法：
+
+1. `for-range` 循环：使用`for`语句和范围表达式来遍历容器中的元素。`for-range` 循环可以用于遍历任何支持迭代器的容器，例如`std::vector`、`std::list`、`std::set`、`std::map`等。
+
+```c++
+std::vector<int> v = { 1, 2, 3 };
+for (int i : v) {
+    std::cout << i << '\n';
+}
+```
+
+1. `std::for_each`：使用`std::for_each`算法和lambda表达式来遍历容器中的元素。`std::for_each`算法接受两个参数：要遍历的容器或迭代器范围，以及一个接受元素的lambda表达式。该算法对每个元素调用lambda表达式，并忽略返回值。
+
+```c++
+std::vector<int> v = { 1, 2, 3 };
+std::for_each(v.begin(), v.end(), [](int i) {
+    std::cout << i << '\n';
+});
+```
+
+需要注意的是，以上两种方法都适用于任何支持迭代器的容器，但它们的性能和实现方式可能有所不同，应根据具体情况选择合适的方法。另外，C++11引入的这些方法与之前的遍历方式相比，具有更简洁、清晰和安全的语法，可以减少代码的编写量和错误的发生概率。
+
+
+
--- cpp_stl/containers/contains.cpp
+++ cpp_stl/containers/contains.cpp
@@ -6,8 +6,319 @@ using namespace std;
 #include <iostream>
 #include <ctime>
 #include <cstdlib> //qsort, bsearch, NULL
+#include <memory.h>
+
+
+
+// 以下 MyString 是為了測試 containers with moveable elements 效果.
+class MyString
+{
+public:
+    static size_t DCtor; // 累計 default-ctor 的呼叫次數
+    static size_t Ctor;  // 累計 ctor      的呼叫次數
+    static size_t CCtor; // 累計 copy-ctor 的呼叫次數
+    static size_t CAsgn; // 累計 copy-asgn 的呼叫次數
+    static size_t MCtor; // 累計 move-ctor 的呼叫次數
+    static size_t MAsgn; // 累計 move-asgn 的呼叫次數
+    static size_t Dtor;  // 累計 dtor 的呼叫次數
+private:
+    char *_data;
+    size_t _len;
+    void _init_data(const char *s)
+    {
+        _data = new char[_len + 1];
+        memcpy(_data, s, _len);
+        _data[_len] = '\0';
+    }
+
+public:
+    // default ctor
+    MyString() : _data(NULL), _len(0) { ++DCtor; }
+
+    // ctor
+    MyString(const char *p) : _len(strlen(p))
+    {
+        ++Ctor;
+        _init_data(p);
+    }
+
+    // copy ctor
+    MyString(const MyString &str) : _len(str._len)
+    {
+        ++CCtor;
+        _init_data(str._data); // COPY
+    }
+
+    // move ctor, with "noexcept"
+    MyString(MyString &&str) noexcept : _data(str._data), _len(str._len)
+    {
+        ++MCtor;
+        str._len = 0;
+        str._data = NULL; // 避免 delete (in dtor)
+    }
+
+    // copy assignment
+    MyString &operator=(const MyString &str)
+    {
+        ++CAsgn;
+        if (this != &str)
+        {
+            if (_data)
+                delete _data;
+            _len = str._len;
+            _init_data(str._data); // COPY!
+        }
+        else
+        {
+            // Self Assignment, Nothing to do.
+        }
+        return *this;
+    }
+
+    // move assignment
+    MyString &operator=(MyString &&str) noexcept
+    {
+        ++MAsgn;
+        if (this != &str)
+        {
+            if (_data)
+                delete _data;
+            _len = str._len;
+            _data = str._data; // MOVE!
+            str._len = 0;
+            str._data = NULL; // 避免 deleted in dtor
+        }
+        return *this;
+    }
+
+    // dtor
+    virtual ~MyString()
+    {
+        ++Dtor;
+        if (_data)
+        {
+            delete _data;
+        }
+    }
+
+    bool
+    operator<(const MyString &rhs) const // 為了讓 set 比較大小
+    {
+        return std::string(this->_data) < std::string(rhs._data); // 借用事實：string 已能比較大小.
+    }
+    bool
+    operator==(const MyString &rhs) const // 為了讓 set 判斷相等.
+    {
+        return std::string(this->_data) == std::string(rhs._data); // 借用事實：string 已能判斷相等.
+    }
+
+    char *get() const { return _data; }
+};
+size_t MyString::DCtor = 0;
+size_t MyString::Ctor = 0;
+size_t MyString::CCtor = 0;
+size_t MyString::CAsgn = 0;
+size_t MyString::MCtor = 0;
+size_t MyString::MAsgn = 0;
+size_t MyString::Dtor = 0;
+namespace std // 必須放在 std 內
+{
+    template <>
+    struct hash<MyString> // 這是為了 unordered containers
+    {
+        size_t
+        operator()(const MyString &s) const noexcept
+        {
+            return hash<string>()(string(s.get()));
+        }
+        // 借用現有的 hash<string> (in ...\include\c++\bits\basic_string.h)
+    };
+}
+//-----------------
+// 以下 MyStrNoMove 是為了測試 containers with no-moveable elements 效果.
+class MyStrNoMove
+{
+public:
+    static size_t DCtor; // 累計 default-ctor 的呼叫次數
+    static size_t Ctor;  // 累計 ctor      的呼叫次數
+    static size_t CCtor; // 累計 copy-ctor 的呼叫次數
+    static size_t CAsgn; // 累計 copy-asgn 的呼叫次數
+    static size_t MCtor; // 累計 move-ctor 的呼叫次數
+    static size_t MAsgn; // 累計 move-asgn 的呼叫次數
+    static size_t Dtor;  // 累計 dtor 的呼叫次數
+private:
+    char *_data;
+    size_t _len;
+    void _init_data(const char *s)
+    {
+        _data = new char[_len + 1];
+        memcpy(_data, s, _len);
+        _data[_len] = '\0';
+    }
+
+public:
+    // default ctor
+    MyStrNoMove() : _data(NULL), _len(0)
+    {
+        ++DCtor;
+        _init_data("jjhou");
+    }
+
+    // ctor
+    MyStrNoMove(const char *p) : _len(strlen(p))
+    {
+        ++Ctor;
+        _init_data(p);
+    }
+
+    // copy ctor
+    MyStrNoMove(const MyStrNoMove &str) : _len(str._len)
+    {
+        ++CCtor;
+        _init_data(str._data); // COPY
+    }
+
+    // copy assignment
+    MyStrNoMove &operator=(const MyStrNoMove &str)
+    {
+        ++CAsgn;
+
+        if (this != &str)
+        {
+            if (_data)
+                delete _data;
+            _len = str._len;
+            _init_data(str._data); // COPY!
+        }
+        else
+        {
+            // Self Assignment, Nothing to do.
+        }
+        return *this;
+    }
+
+    // dtor
+    virtual ~MyStrNoMove()
+    {
+        ++Dtor;
+        if (_data)
+        {
+            delete _data;
+        }
+    }
+
+    bool
+    operator<(const MyStrNoMove &rhs) const // 為了讓 set 比較大小
+    {
+        return string(this->_data) < string(rhs._data); // 借用事實：string 已能比較大小.
+    }
+
+    bool
+    operator==(const MyStrNoMove &rhs) const // 為了讓 set 判斷相等.
+    {
+        return string(this->_data) == string(rhs._data); // 借用事實：string 已能判斷相等.
+    }
+
+    char *get() const { return _data; }
+};
+size_t MyStrNoMove::DCtor = 0;
+size_t MyStrNoMove::Ctor = 0;
+size_t MyStrNoMove::CCtor = 0;
+size_t MyStrNoMove::CAsgn = 0;
+size_t MyStrNoMove::MCtor = 0;
+size_t MyStrNoMove::MAsgn = 0;
+size_t MyStrNoMove::Dtor = 0;
+
+namespace std // 必須放在 std 內
+{
+    template <>
+    struct hash<MyStrNoMove> // 這是為了 unordered containers
+    {
+        size_t
+        operator()(const MyStrNoMove &s) const noexcept
+        {
+            return hash<string>()(string(s.get()));
+        }
+        // 借用現有的 hash<string> (in ...\4.9.2\include\c++\bits\basic_string.h)
+    };
+}
 
 const long ASIZE = 500000L;
+#include <ctime> //clock_t, clock()
+//--------------------------------------------------
+#include <typeinfo>  //typeid()
+template<typename T>
+void output_static_data(const T& myStr)
+{
+	cout << typeid(myStr).name() << " -- " << endl; 
+	cout << " CCtor=" << T::CCtor 	
+		 << " MCtor=" << T::MCtor 
+	     << " CAsgn=" << T::CAsgn 		 
+		 << " MAsgn=" << T::MAsgn 
+		 << " Dtor="  << T::Dtor 
+		 << " Ctor="  << T::Ctor 
+		 << " DCtor=" << T::DCtor 		 
+		 << endl;	
+}
+
+template <typename M, typename NM>
+void test_moveable(M c1, NM c2, long &value)
+{
+    char buf[10];
+
+    // 測試 move
+    cout << "\n\ntest, with moveable elements" << endl;
+    typedef typename iterator_traits<typename M::iterator>::value_type V1type;
+    clock_t timeStart = clock();
+    for (long i = 0; i < value; ++i)
+    {
+        snprintf(buf, 10, "%d", rand());
+        auto ite = c1.end();
+        c1.insert(ite, V1type(buf));
+    }
+    cout << "construction, milli-seconds : " << (static_cast<double>(clock() - timeStart) / CLOCKS_PER_SEC * 1000) << endl;
+    cout << "size()= " << c1.size() << endl;
+    output_static_data(*(c1.begin()));
+
+    timeStart = clock();
+    M c11(c1);
+    cout << "copy, milli-seconds : " << (static_cast<double>(clock() - timeStart) / CLOCKS_PER_SEC * 1000) << endl;
+
+    timeStart = clock();
+    M c12(std::move(c1));
+    cout << "move copy, milli-seconds : " << (static_cast<double>(clock() - timeStart) / CLOCKS_PER_SEC * 1000) << endl;
+
+    timeStart = clock();
+    c11.swap(c12);
+    cout << "swap, milli-seconds : " << (static_cast<double>(clock() - timeStart) / CLOCKS_PER_SEC * 1000) << endl;
+
+    // 測試 non-moveable
+    cout << "\n\ntest, with non-moveable elements" << endl;
+    typedef typename iterator_traits<typename NM::iterator>::value_type V2type;
+    timeStart = clock();
+    for (long i = 0; i < value; ++i)
+    {
+        snprintf(buf, 10, "%d", rand());
+        auto ite = c2.end();
+        c2.insert(ite, V2type(buf));
+    }
+
+    cout << "construction, milli-seconds : " << (static_cast<double>(clock() - timeStart) / CLOCKS_PER_SEC * 1000) << endl;
+    cout << "size()= " << c2.size() << endl;
+    output_static_data(*(c2.begin()));
+
+    timeStart = clock();
+    NM c21(c2);
+    cout << "copy, milli-seconds : " << (static_cast<double>(clock() - timeStart) / CLOCKS_PER_SEC * 1000) << endl;
+
+    timeStart = clock();
+    NM c22(std::move(c2));
+    cout << "move copy, milli-seconds : " << (static_cast<double>(clock() - timeStart) / CLOCKS_PER_SEC * 1000) << endl;
+
+    timeStart = clock();
+    c21.swap(c22);
+    cout << "swap, milli-seconds : " << (static_cast<double>(clock() - timeStart) / CLOCKS_PER_SEC * 1000) << endl;
+}
 //-----------------
 long get_a_target_long()
 {
@@ -80,7 +391,85 @@ namespace jj01
             cout << "not found! " << endl;
     }
 }
+//---------------------------------------------------
+#include <vector>
+#include <stdexcept>
+#include <string>
+#include <cstdlib> //abort()
+#include <cstdio>  //snprintf()
+#include <iostream>
+#include <ctime>
+#include <algorithm> //sort()
+namespace jj02
+{
+    void test_vector(long &value)
+    {
+        cout << "\ntest_vector().......... \n";
 
+        vector<string> c;
+        char buf[10];
+
+        clock_t timeStart = clock();
+        for (long i = 0; i < value; ++i)
+        {
+            try
+            {
+                snprintf(buf, 10, "%d", rand());
+                c.push_back(string(buf));
+            }
+            catch (exception &p)
+            {
+                cout << "i=" << i << " " << p.what() << endl;
+                // 曾經最高 i=58389486 then std::bad_alloc
+                abort();
+            }
+        }
+        double elapsedMilliseconds = static_cast<double>(clock() - timeStart) / CLOCKS_PER_SEC * 1000;
+        cout << "milli-seconds : " << elapsedMilliseconds << endl;
+        cout << "vector.max_size()= " << c.max_size() << endl; // 1073747823
+        cout << "vector.size()= " << c.size() << endl;
+        cout << "vector.front()= " << c.front() << endl;
+        cout << "vector.back()= " << c.back() << endl;
+        cout << "vector.data()= " << c.data() << endl;
+        cout << "vector.capacity()= " << c.capacity() << endl
+             << endl;
+
+        string target = get_a_target_string();
+        {
+            timeStart = clock();
+            auto pItem = find(c.begin(), c.end(), target);
+            cout << "std::find(), milli-seconds : " << (clock() - timeStart) << endl;
+
+            if (pItem != c.end())
+                cout << "found, " << *pItem << endl
+                     << endl;
+            else
+                cout << "not found! " << endl
+                     << endl;
+        }
+
+        {
+            timeStart = clock();
+            sort(c.begin(), c.end());
+            cout << "sort(), milli-seconds : " << (static_cast<double>(clock() - timeStart) / CLOCKS_PER_SEC * 1000) << endl;
+
+            timeStart = clock();
+            string *pItem = (string *)::bsearch(&target, (c.data()),
+                                                c.size(), sizeof(string), compareStrings);
+            cout << "bsearch(), milli-seconds : " << (static_cast<double>(clock() - timeStart) / CLOCKS_PER_SEC * 1000) << endl;
+
+            if (pItem != NULL)
+                cout << "found, " << *pItem << endl
+                     << endl;
+            else
+                cout << "not found! " << endl
+                     << endl;
+        }
+
+        c.clear();
+        test_moveable(vector<MyString>(), vector<MyStrNoMove>(), value);
+    }
+}
 //---------------------------------------------------
 #include <cstdlib> //rand() and RAND_MAX
 namespace jjxx
