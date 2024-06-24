a541a2d76a9004be49d60b1ca3785833968d5e4d
arno <1017845778@qq.com>
2024-06-24 17:31:24 +0800

    增加zmq node测试代码

--- opensource_test/protobus/protobus/src/protobus.cpp
+++ opensource_test/protobus/protobus/src/protobus.cpp
@@ -1,77 +1,246 @@
 #include "zmq/zmq.hpp"
-#include <vector>
 #include "protobus.h"
 #include <iostream>
 #include <thread>
 #include <mutex>
 #include <condition_variable>
 #include <stdexcept>
-#include "message.pb.h"
+#include <queue>
 #include "google/protobuf/util/time_util.h"
+#include "message.pb.h"
+#include <unordered_map>
 using namespace std;
+using google::protobuf::Timestamp;
+using google::protobuf::util::TimeUtil;
 
-const char *TCP_SUB = "tcp://127.0.0.1:5555";
-const char *TCP_PUB = "tcp://127.0.0.1:5556";
+uint8_t sendBuf[65535];
 struct protobus_handle
 {
-    /* psb socket */
+    /* pub socket */
     zmq::socket_t *pub;
     /* sub socket */
     zmq::socket_t *sub;
-    /* topic vec */
-    vector<string> topic;
+    /* sub ctx */
+    zmq::context_t *sub_ctx;
     /* sender identify*/
     string identify;
     /* run status */
     bool isRunning = false;
-
-    std::mutex mutex;
-
-    std::condition_variable cond;
-
-    std::vector<MSG::WrapperMessage> msgVec;
+    /* topic */
+    std::mutex topicMutex;
+    std::condition_variable topicCond;
+    /* topic map */
+    std::unordered_map<string, protobus_cb> topicMap;
+    /* protobuf msg */
+    std::queue<std::shared_ptr<MSG::WrapperMessage>> msgQueue;
+    std::mutex queueMutex;
+    std::condition_variable queueCond;
 };
-void pub_task(protobus_handle_t *h)
+static void subscriber_init(protobus_handle_t *handle);
+static size_t send_msg(protobus_handle_t *handle, std::shared_ptr<MSG::WrapperMessage> msg)
 {
-    zmq::context_t ctx(1);
-    h->pub = new zmq::socket_t(ctx, zmq::socket_type::pub);
-    h->pub->connect(TCP_PUB);
-
-    if (h->isRunning)
+    std::unique_ptr<uint8_t[]> dataBuf;
+    uint8_t *bufPtr;
+    bool releaseFlag = false;
+    size_t sendSize = msg->ByteSizeLong();
+    std::cout << "send topic " << msg->topic() << endl;
+    if (sendSize > sizeof(sendBuf))
     {
-        std::unique_lock<mutex> lock(h->mutex);
-        h->cond.wait(lock, [&h]
-                     { return !h->msgVec.empty(); });
+        dataBuf = std::make_unique<uint8_t[]>(sendSize + 1);
+        std::fill(dataBuf.get(), dataBuf.get() + sendSize + 1, 0);
+        bufPtr = dataBuf.get();
+    }
+    else
+    {
+        bufPtr = sendBuf;
+    }
+
+    try
+    {
+        zmq::message_t topic(msg->topic());
+        handle->pub->send(topic, zmq::send_flags::sndmore);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << "send proto topic\n";
+        return -1;
+    }
+
+    try
+    {
+        Timestamp timestamp;
+        timestamp.set_seconds(time(NULL));
+        timestamp.set_nanos(0);
+        *msg->mutable_timestamp() = timestamp;
+
+        msg->SerializePartialToArray(bufPtr, sendSize);
+        zmq::message_t zmq_msg(bufPtr, sendSize);
+
+        handle->pub->send(zmq_msg, zmq::send_flags::dontwait);
+        std::cout << "send msg" << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << "send proto message\n";
+        return -1;
+    }
+    return sendSize;
+}
+static std::shared_ptr<MSG::WrapperMessage> get_msg(protobus_handle_t *handle)
+{
+    std::unique_lock<mutex> lock(handle->queueMutex);
+    handle->queueCond.wait(lock, [&handle]
+                           {std::cout << "handle->msgQueue.size() = "<< handle->msgQueue.size() << std::endl; 
+                            return !handle->msgQueue.empty(); });
+    auto msgPtr = handle->msgQueue.front();
+    std::cout << "queue size " << handle->msgQueue.size() << std::endl;
+    handle->msgQueue.pop();
+    lock.unlock();
+    return msgPtr;
+}
+static void sub_task(protobus_handle_t *handle)
+{
+    
+    printf("start sub task\n");
+    while (handle->isRunning)
+    {
+
+        std::unique_lock<mutex> lock(handle->topicMutex);
+        handle->queueCond.wait(lock, [&handle]
+                               { return !handle->topicMap.empty(); });
         lock.unlock();
+
+        zmq::message_t topic;
+        zmq::message_t zmq_msg;
+
+        zmq::recv_result_t result = handle->sub->recv(topic);
+        if (result.has_value())
+        {
+            std::string topic(static_cast<char *>(topic.data()), topic.size());
+            auto it = handle->topicMap.find(topic);
+            if (it != handle->topicMap.end())
+            {
+                result = handle->sub->recv(zmq_msg, zmq::recv_flags::none);
+                if (result.has_value())
+                {
+                    MSG::WrapperMessage wrapper_msg;
+                    wrapper_msg.ParseFromArray(zmq_msg.data(), zmq_msg.size());
+                    it->second(wrapper_msg);
+                }
+            }
+        }
+        else
+        {
+            continue;
+        }
+    }
+    handle->sub->close();
+    handle->sub = nullptr;
+}
+static void pub_task(protobus_handle_t *handle)
+{
+    size_t sendSize = 0;
+    zmq::context_t ctx(1);
+    handle->pub = new zmq::socket_t(ctx, zmq::socket_type::pub);
+    handle->pub->connect(TCP_SUB);
+    printf("start pub task\n");
+    while (handle->isRunning)
+    {
+        auto msgPtr = get_msg(handle);
+        if (msgPtr != nullptr)
+        {
+            std::cout << "get msg ,start send" << std::endl;
+            sendSize = send_msg(handle, msgPtr);
+            if (sendSize != msgPtr->ByteSizeLong())
+            {
+                std::cerr << "send msg failed ,ret %d" << sendSize << std::endl;
+            }
+        }
     }
 }
 protobus_handle_t *protobus_init(const char *node_name)
 {
-    protobus_handle_t *h = nullptr;
+    protobus_handle_t *handle = nullptr;
     try
     {
-        h = new protobus_handle_t();
+        handle = new protobus_handle_t();
 
-        h->pub = nullptr;
-        h->sub = nullptr;
+        handle->pub = nullptr;
+        handle->sub = nullptr;
 
         if (node_name != nullptr)
         {
-            h->identify = string(node_name);
+            handle->identify = string(node_name);
         }
-        h->isRunning = true;
-        thread t(pub_task, h);
+        handle->isRunning = true;
+        thread t(pub_task, handle);
         t.detach();
+        subscriber_init(handle);
+        printf("%d subscriber_init \n", __LINE__);
     }
     catch (const std::exception &e)
     {
         cerr << "Error: " << e.what() << endl;
-        delete h;
-        h = nullptr;
+        delete handle;
+        handle = nullptr;
     }
-    return h;
+    printf("%d return handle %p \n", __LINE__, handle);
+    return handle;
+}
+protobus_handle_t *protobus_init(const char *node_name, vector<string> topics, protobus_cb cb)
+{
+    protobus_handle_t *handle = protobus_init(node_name);
+    if (handle != nullptr)
+    {
+        for (auto it = topics.begin(); it != topics.end(); it++)
+        {
+
+            printf("add sub topic %s\n", (*it).data());
+            protobus_add_subscriber(handle, (*it).data(), cb);
+        }
+    }
+    return handle;
 }
 void protobus_cleanup(protobus_handle_t *handle)
 {
     delete handle;
+}
+void protobus_send(protobus_handle_t *handle, const MSG::WrapperMessage &msg)
+{
+    std::lock_guard<std::mutex> lock(handle->queueMutex);
+    handle->msgQueue.push(std::make_shared<MSG::WrapperMessage>(msg));
+    std::cout << "push " << msg.topic() << std::endl;
+    handle->queueCond.notify_one();
+}
+static void subscriber_init(protobus_handle_t *handle)
+{
+    handle->sub_ctx = new zmq::context_t(1);
+    handle->sub = new zmq::socket_t(*handle->sub_ctx, zmq::socket_type::sub);
+    handle->sub->connect(TCP_PUB);
+    thread t(sub_task, handle);
+    t.detach();
+    printf("%d subscriber_init \n", __LINE__);
+}
+void protobus_add_subscriber(protobus_handle_t *handle, const char *topic, protobus_cb cb)
+{
+    printf("handle %p\n", handle);
+    std::lock_guard<std::mutex> lock(handle->topicMutex);
+    handle->topicMap[topic] = cb;
+    handle->sub->set(zmq::sockopt::subscribe, topic);
+    handle->topicCond.notify_one();
+}
+void protobus_del_subscriber(protobus_handle_t *handle, const char *topic)
+{
+    std::lock_guard<std::mutex> lock(handle->topicMutex);
+    auto it = handle->topicMap.find(topic);
+    if (it != handle->topicMap.end())
+    {
+        handle->topicMap.erase(it);
+        std::cout << "topic " << topic << "' removed from map." << std::endl;
+    }
+    else
+    {
+        std::cout << "topic " << topic << "' not found in map." << std::endl;
+    }
+    handle->sub->set(zmq::sockopt::unsubscribe, topic);
 }
--- opensource_test/protobus/protobus_node_cpp/src/node.cpp
+++ opensource_test/protobus/protobus_node_cpp/src/node.cpp
@@ -1,14 +1,123 @@
 #include <iostream>
 #include "protobus.h"
 #include <libgen.h>
-
+#include <unistd.h>
+#include "google/protobuf/util/time_util.h"
+#include <sstream>
+#include <iomanip>
+#include <random>
+#include <string>
+#include <thread>
+#include <sys/time.h>
+using namespace std;
+using namespace std::chrono_literals;
+using google::protobuf::Timestamp;
+using google::protobuf::util::TimeUtil;
 protobus_handle_t *handle = nullptr;
+std::vector<std::string> topics;
+std::string timestamp_to_string(uint64_t timestamp)
+{
+    std::time_t time = timestamp / 1000000; // 转换到秒
+    std::stringstream ss;
+    ss << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S");
+    return ss.str();
+}
+void protobus_callback(const MSG::WrapperMessage &msg)
+{
+    uint64_t time = msg.timestamp().seconds() * 1000000 + msg.timestamp().nanos();
+    switch (msg.message_type_case())
+    {
+    case MSG::WrapperMessage::kPeople:
+    {
+        const MSG::msg_people &people_msg = msg.people();
+        std::cout << "[" << timestamp_to_string(time) << "] "
+                  << "Received message on topic " << msg.topic()
+                  << ": Name = " << people_msg.name()
+                  << ", Age = " << people_msg.age() << std::endl;
+    }
+    break;
+    case MSG::WrapperMessage::kAddress:
+    {
+        const MSG::msg_address &addr = msg.address();
+        std::cout << "[" << timestamp_to_string(time) << "] "
+                  << "Received message on topic " << msg.topic()
+                  << ": City = " << addr.city()
+                  << ", Street = " << addr.street() << std::endl;
+    }
+    break;
+    default:
+    {
+        std::cout << "unknow topic " << msg.topic();
+    }
+    break;
+    }
+}
 int main(int argc, char **argv)
 {
-    std::cout << "cpp node ...." << std::endl;
-    handle = protobus_init(basename(argv[0]));
+    int nodeType = 0;
+    if(strcmp(argv[1], "sub") == 0)
+    {
+        nodeType = 0;
+    }
+    else
+    {
+        nodeType = 1;
+    }
 
-    std::cout << "name:" << basename(argv[0]) << std::endl;
+    std::cout << argv[1] << " node ...." << std::endl;
+    if (nodeType == 0)
+    {
+        for (int i = 2; i < argc; i++)
+        {
+            std::string str = "argv[" + std::to_string(i) + "] = ";
+            std::cout << str << argv[i] << std::endl;
+            topics.push_back(argv[i]);
+        }
+
+        handle = protobus_init(basename(argv[0]), topics, protobus_callback);
+    }
+    else
+    {
+        handle = protobus_init(basename(argv[0]));
+    }
+    while (1)
+    {
+        sleep(1);
+        if (nodeType == 1)
+        {
+            printf("start send topic\n");
+            std::random_device rd;
+            std::mt19937 mt(rd());
+            std::uniform_int_distribution<int> dist(0, 100); // 0到100的随机数
+            MSG::WrapperMessage wrapper_msg;
+            MSG::msg_people *people_msg;
+            MSG::msg_address *addr_msg;
+            const int randomNum = dist(mt);
+            if (randomNum % 2 == 0)
+            {
+                std::cout << "send address" << std::endl;
+                wrapper_msg.set_topic("address");
+                addr_msg = wrapper_msg.mutable_address();
+                addr_msg->set_city("abc");
+                addr_msg->set_street("567");
+            }
+            else
+            {
+                std::cout << "send people" << std::endl;
+                people_msg = new MSG::msg_people();
+                people_msg->set_name("arno");
+                people_msg->set_age(20);
+                wrapper_msg.set_topic("people");
+                wrapper_msg.set_allocated_people(people_msg);
+            }
+            Timestamp timestamp;
+            timestamp.set_seconds(time(NULL));
+            timestamp.set_nanos(0);
+            *wrapper_msg.mutable_timestamp() = timestamp;
+            protobus_send(handle, wrapper_msg);
+        }
+    }
     protobus_cleanup(handle);
+
     return 0;
 }
