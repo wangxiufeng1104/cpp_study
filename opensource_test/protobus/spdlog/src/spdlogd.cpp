2783717ff594d5333eaf2b0e061f62a2a16ee2a9
arno <1017845778@qq.com>
2024-06-26 15:37:34 +0800

    【spdlog】添加spdlog日志库

---
+++ opensource_test/protobus/spdlog/src/spdlogd.cpp
@@ -0,0 +1,47 @@
+// 输出格式请参考https://github.com/gabime/spdlog/wiki/3.-Custom-formatting
+#include <cstdio>
+#include <chrono>
+#include <iostream>
+#define SPDLOG_NAME "spdlog"
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE // 必须定义这个宏,才能输出文件名和行号
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/rotating_file_sink.h"
+#include "spdlog/sinks/rotating_file_sink.h"
+#include "spdlog/fmt/bin_to_hex.h"
+#include <signal.h>
+#include <libgen.h>
+#include "protobus.hpp"
+
+using namespace std;
+struct ubus_context *ctx;
+uint32_t exit_flag = 1;
+// 智能指针
+std::shared_ptr<spdlog::logger> rotating_logger;
+
+void signal_hander(int sig)
+{
+    printf("signal %d\n", sig);
+    exit_flag = 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+    // Create a file rotating logger with 5mb size max and 3 rotated files
+    int ret = 0;
+    signal(SIGTERM, signal_hander);
+    signal(SIGINT, signal_hander);
+    auto max_size = 1024 * 1024 * 100;
+    auto max_files = 4;
+    rotating_logger = spdlog::rotating_logger_mt(SPDLOG_NAME, "/tmp/log/log.txt", max_size, max_files);
+    spdlog::flush_every(std::chrono::seconds(1));
+    spdlog::set_default_logger(rotating_logger);
+    spdlog::set_level(spdlog::level::debug);
+    spdlog::set_pattern("%v");
+    protobus* bus = protobus::get_instance(basename(argv[0]));
+    
+    while (exit_flag)
+    {
+        sleep(1);
+    }
+}
