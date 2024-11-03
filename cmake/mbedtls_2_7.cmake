2f823add92dcee096ede15ea829db3a1ceb1a9c0
arno <1017845778@qq.com>
2024-11-03 20:41:36 +0800

    优化代码组织结构

---
+++ cmake/mbedtls_2_7.cmake
@@ -0,0 +1,14 @@
+include(FetchContent)
+
+# FetchContent_Declare(mbedtls
+#         GIT_REPOSITORY https://github.com/Mbed-TLS/mbedtls.git
+#         GIT_TAG v3.6.2)
+
+fetchcontent_declare(mbedtls
+        URL https://github.com/Mbed-TLS/mbedtls/archive/refs/tags/v2.7.16.zip
+        SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/ext/mbedtls # 指定库下载地址
+        )
+# 设置选项以禁用测试和示例代码
+set(ENABLE_TESTING OFF CACHE BOOL "Disable building testing code")
+set(ENABLE_PROGRAMS OFF CACHE BOOL "Disable building example code")
+fetchcontent_makeavailable(mbedtls)
